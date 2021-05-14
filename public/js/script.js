9e8b7e31fdc470b7b56a59802bd4877c2c68994f
pouya haghighi <pouyahaghighi@pouyas-MacBook-Air.local>
2021-05-14 21:12:09 +0430

    Init commit

--- public/js/script.js
+++ public/js/script.js
@@ -9,7 +9,7 @@
   let startStreaming = false;
   
   const [start, screenshot] = buttons;
-  $('body').style.background = 'url("/img/4.jpg") no-repeat center center fixed';
+  $('body').style.background = 'url("/img/4.webp") no-repeat center center fixed';
   const videoConstraints = {
     video: {
       width: {
